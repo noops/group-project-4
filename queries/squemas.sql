9ba4077298b477c85536d1edb776415216640778
ines-tb <ines12tb@gmail.com>
2020-10-17 18:08:26 -0500

    Raw data DB load + gitignore file

--- queries/squemas.sql
+++ queries/squemas.sql
@@ -1,17 +1,17 @@
 -- Creation of incident_info table
 
 CREATE TABLE incident_info (
-	IncidentNum int,
-	Category character varying(50),
-	Description character varying(100),
-	DayOfWeek character varying(10),
+	"IncidntNum" int,
+	"Category" character varying(50),
+	"Descript" character varying(100),
+	"DayOfWeek" character varying(10),
 	"Date" date,
 	"Time" time,
-	PdDistrict character varying(50),
-	Resolution character varying(50),
-	Address character varying(100),
-	Latitude numeric(15,12),
-	Longitude numeric(15,12),
-	Location character varying(50),
-	PdId bigint
-);	
+	"PdDistrict" character varying(50),
+	"Resolution" character varying(50),
+	"Address" character varying(100),
+	"X" numeric(15,12),
+	"Y" numeric(15,12),
+	"Location" character varying(50),
+	"PdId" bigint
+);
