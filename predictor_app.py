8702807a6c96699c1aa5900fbc696b8f3a66db86
Benjamin Corde Kirton <bkirton@benjamins-mbp-3.lan>
2020-11-04 14:14:47 -0600

    working on flask app.

--- predictor_app.py
+++ predictor_app.py
@@ -1,36 +1,50 @@
-from flask import Flask, request, render_template
+import flask
+from flask import render_template
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
+import pandas as pd
 import pickle
 
 #initialize the app
-app = Flask(__name__)
+app = flask.Flask(__name__)
 #load the model
 model = pickle.load(open('model.pkl', 'rb'))
 
 
 #redirect the api to homepage
-@app.route('/')
-def home():
-    return render_template('SFCrime.html')
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if flask.request.method == 'GET':
+        return render_template('index.html')
+    
+    if flask.request.method == 'POST':
+        #extract user input from webpage
+        day_of_week = flask.request.form['Weekdays']
+        time_of_day = flask.request.form['ToD']
+        police_district = flask.request.form['District']
+        crime_category = flask.request.form['Category']
 
-#redirect api to predict result (arrest or release)
-@app.route('/predict', methods=['POST'])
-def predict():
+        #create dataframe for model
+        input_variables = pd.DataFrame([[day_of_week, time_of_day, police_district, crime_category]], columns=['Dow', 'Tod', 'district', 'category'], dtype=str, index=['index'])
+        dummy_df = pd.get_dummies(input_variables)
+        #get models prediction
+        prediction = model.predict(dummy_df)[0]
+    
+        def final_result(prediction):
+            if prediction == 0:
+                jail_time_text = "You've been arrested."
+                return jail_time_text
+            else: 
+                no_jail_time_text = "It's your lucky day."
+                return no_jail_time_text
+            
+            
 
-    #take user input from dashboard and get a prediction 
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int)]
-    prediction = model.predict(final_features)
+        return render_template('index.html', prediction_text = final_result )
 
-    def final_result(prediction):
-        if prediction == 0:
-            jail_time_text = "You've been arrested."
-            return jail_time_text
-        else: 
-            no_jail_time_text = "It's your lucky day."
-            return no_jail_time_text
-        
-        
 
-    return render_template('SFCrime.html', prediction_text = final_result )
+
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
