4648a2424fb59fc665ba9290b05abc444e9dc8ee
Benjamin Corde Kirton <bkirton@benjamins-mbp-3.lan>
2020-11-11 13:23:34 -0600

    updated html and replaced model with new improved randomForest

--- dashboardApp/app.py
+++ dashboardApp/app.py
@@ -5,12 +5,15 @@ import numpy as np
 import pandas as pd
 import pickle
 import urllib.request
-
+import re 
 # with urllib.request.urlopen('https://group4ds-bucket.s3.amazonaws.com/model.pkl') as response:
 #     pickle = response.read()
 
-url = 'https://group4ds-bucket.s3.amazonaws.com/model.pkl'
-urllib.request.urlretrieve(url, 'model.pkl')
+# url = 'https://group4ds-bucket.s3.amazonaws.com/model.pkl'
+# urllib.request.urlretrieve(url, 'model.pkl')
+model = pickle.load(open('model.pkl', 'rb'))
+df = pd.read_csv('./Resources/encoded_df.csv')
+available_zips = df['ZipCode'].apply(str).unique()
 
 # initialize the app
 app = flask.Flask(__name__)
@@ -39,33 +42,49 @@ def main():
         category = {'16': 'WEAPON LAWS', '15': 'WARRANTS', '6': 'NON-CRIMINAL', '0': 'ASSAULT', '8': 'OTHER OFFENSES', '5': 'MISSING PERSON', '4': 'LARCENY/THEFT', '1': 'BURGLARY',
                     '7': 'OTHER', '9': 'ROBBERY', '3': 'FRAUD', '2': 'DRUG/NARCOTIC', '14': 'VEHICLE THEFT', '13': 'VANDALISM', '10': 'SECONDARY CODES', '11': 'SUSPICIOUS OCC', '12': 'TRESPASS'}
 
-        # extract user input from webpage
+        #extract user input from webpage
         day_of_week = flask.request.form['Weekdays']
         time_of_day = flask.request.form['ToD']
         police_district = flask.request.form['District']
         crime_category = flask.request.form['Category']
+        zip_code = flask.request.form['ZipCode']
 
-        # create dataframe for model
-        input_variables = pd.DataFrame([[day_of_week, time_of_day, police_district, crime_category]], columns=[
-                                       'Dow', 'Tod', 'district', 'category'], dtype=str, index=['index'])
+        warning=""
+        if (not re.match("941\\d{2}",zip_code)):
+            warning=f"'{zip_code}' doesn't match SFO ZipCode"
+            
 
-        # get models prediction
-        prediction = model.predict(input_variables)
+        # available_zips=['94103', '94124', '94108', '94102', '94109', '94158', '94122', '94116', '94112', '94104', '94110', '94132', '94114', '94131', '94134', '94117', '94115', '94105', '94127', '94118', '94111', '94123', '94107', '94130', '94129']
+        
+        if zip_code in available_zips:
 
-        print(prediction)
+            #create dataframe for model
+            input_variables = pd.DataFrame([[day_of_week, time_of_day, police_district, crime_category, zip_code]], columns=['Dow', 'Tod', 'district', 'category', 'zip'], dtype=str, index=['index'])
 
-        if prediction == 1:
-            outcome = "You've committed a crime and gotten away"
+            #get models prediction
+            prediction = model.predict(input_variables)
+
+            print(prediction)
+                
+                
+            if prediction == 1:
+                outcome = "You've committed a crime and gotten away"
+            else:
+                outcome = "You've most likely been arrested"
+        
         else:
-            outcome = "You've most likely been arrested"
+            
+            outcome = "No available data for introduced ZipCode."
 
-        # day.get(day_of_week)
+        # Print out final result
         return render_template('index.html',
-                               original_input={'Day of Week': day.get(day_of_week),
-                                               'Time of Day': time.get(time_of_day),
-                                               'Police District': pddistrict.get(police_district),
-                                               'Crime Category': category.get(crime_category)},
-                               result=outcome)
+        warning=warning, 
+        original_input={'Day of Week': day.get(day_of_week), 
+                        'Time of Day': time.get(time_of_day), 
+                        'Police District': pddistrict.get(police_district), 
+                        'Crime Category': category.get(crime_category),
+                        'Zip Code': zip_code}, 
+        result = outcome )  
 
 
 if __name__ == "__main__":
